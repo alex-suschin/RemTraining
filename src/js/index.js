4c0390c7982f630982f3c140d813a7c51bd5ea01
alex-suschin <alex_suschin@mail.ru>
2020-09-07 18:11:34 +0300

    first commit

---
+++ src/js/index.js
@@ -0,0 +1,29 @@
+import $ from "jquery";
+import "./import/modules";
+import "../../node_modules/bootstrap/js/dist/modal";
+import "./import/jquery.nice-select.min";
+import "./import/jquery.mask";
+
+$(function() {
+
+
+    $("input[type=tel]").mask("+7 (999) 999-9999");
+
+    $('select').niceSelect();
+
+    $('.scrollto').click(function() {
+        var scroll_elem = $(this).attr('href');
+        if ($(scroll_elem).length != 0) {
+            $('html, body').animate({
+                scrollTop: $(scroll_elem).offset().top - 100
+            }, 800);
+        } else {
+            $('html, body').animate({
+                scrollTop: 0
+            }, 800);
+        }
+        return false;
+    });
+
+
+});
